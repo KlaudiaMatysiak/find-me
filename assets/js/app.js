b019f9f95931730664866c94ba8fbb1d20677f84
KlaudiaMatysiak <klatysiak@gmail.com>
2021-11-05 11:33:12 +0000

    Intial plan for programming a game

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// Create array with images for the game
+
+
+// Create random board
+
+
+// Create random main picture
+
+
+// Check for matches
+
+
+// Flip the pic
+
+
+// Start the game
+
+
+// Timer
