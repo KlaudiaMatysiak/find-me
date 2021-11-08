b0b190708db0ee0dbd7d4eafcecccbe5aa29711f
KlaudiaMatysiak <klatysiak@gmail.com>
2021-11-08 17:46:16 +0000

    Add functions to create a randomize board game and main pic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,65 @@
+document.addEventListener('DOMContentLoaded', initializeGame);
+
 // Create array with images for the game
+const picsArray = [
+    'eagle',
+    'elephant',
+    'cat',
+    'dog',
+    'fox',
+    'hedgehog',
+    'horse',
+    'owl',
+    'turtle'
+]
 
+let grid;
+let currentGamePics;
 
-// Create random board
+function initializeGame() {
+    grid = document.querySelector('.grid');
+    handlePlayClick();
+}
 
+// Start game button
+function handlePlayClick() {
+    const playButton = document.getElementById('start');
+    playButton.addEventListener('click', () => {
+        createBoard();
+        setTimeout(randomizeMainPic, 3000);
+    });
 
-// Create random main picture
+}
+
+// Random main picture
+function randomizeMainPic() {
+    const previewPic = document.querySelector('img.preview');
+    let randomIndex = Math.floor(Math.random() * currentGamePics.length);
+    previewPic.src = `assets/images/${currentGamePics[randomIndex]}.jpg`;
+    console.log(randomIndex);
+}
+
+// Add hide class
+function addHide() {
+    let pictureHide = document.querySelectorAll('picture');
+    pictureHide.classList.add('hide');
+}
+
+// Random nine pictures
+function createBoard() {
+    currentGamePics = picsArray.sort(() => 0.5 - Math.random()).slice(0, 9);
+    currentGamePics.forEach((img, i) => {
+        const element = document.querySelector(`.card:nth-child(${i+1}) img.picture`);
+        element.src = `assets/images/${img}.jpg`;
+        setTimeout(addHide, 2000);
+    })
+}
 
 
 // Check for matches
 
 
 // Flip the pic
-
-
-// Start the game
 
 
 // Timer
